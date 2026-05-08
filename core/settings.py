b54501083ad316caacbdf504c2ad1f5114f7b558
Juancho Santos <jjsantosfernandez02@gmail.com>
2026-05-08 11:31:19 +0200

    cambiando nombre a core

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ import os #para la traduccion
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = BASE_DIR / "e_comerce" / "templates"
+TEMPLATES_DIR = BASE_DIR / "core" / "templates"
 
 
 # Quick-start development settings - unsuitable for production
